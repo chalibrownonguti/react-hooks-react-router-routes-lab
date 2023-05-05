4b9deda466c0db7642928b035a7a76659de25b83
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-05 10:48:54 +0300

    reffered

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>
-    <h1>Actors</h1>
-  </div>;
+  const actorsList= actors.map((act)=>{
+    const moviesList = (act.movies).map((mov)=><li key={Math.floor(Math.random()  * (1000 - 100)) + 100}>{mov}</li>);
+    return (
+      <div>
+         <h1>{act.name}</h1>
+         <ul>
+           {moviesList}
+         </ul>
+      </div>)                                 
+  })
+  return(
+  <div>
+    <h1>Actors Page</h1>
+      {actorsList}
+  </div>)
 }
 
 export default Actors;
--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,29 @@ import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
+
 function App() {
-  return <div>
-    <Home/>
-    <Movies/>
-    <Directors/>
-    <Actors/>
-  </div>;
+
+  return ( 
+  <div>
+    <NavBar />
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path='/directors'>
+        <Directors />
+      </Route>
+      <Route path='/actors'>
+        <Actors />
+      </Route>
+      
+    </Switch>
+  </div>
+  )
 }
 
 export default App;
